a85a4fdde1ee609589c4e60f1399d17ab513c774
Knut Lorvik <lorviken@gmail.com>
2015-05-10 12:02:10 +0200

    Add token fetch

--- src/app/components/userlogin/userlogin.controller.js
+++ src/app/components/userlogin/userlogin.controller.js
@@ -1,11 +1,11 @@
 'use strict';
 
 angular.module('gainmaster')
-  .controller('UserLoginController', function ($scope, UserLoginService, accountFactory) {
+  .controller('UserLoginController', function($scope, UserLoginService, accountFactory, $http, OAuth) {
 
     $scope.username = '';
     $scope.password = '';
-    $scope.submitted      = false;
+    $scope.submitted = false;
     $scope.userData = [];
     var hashedPassword = '';
 
@@ -22,12 +22,23 @@ angular.module('gainmaster')
     };
 
     $scope.getUser = function(ID) {
-      accountFactory.getUser(ID)
-        .then( function (userData){applyRemoteData(userData);
-        });
+      //accountFactory.getUser(ID)
+      //  .then( function (userData){applyRemoteData(userData);
+      //  });
+
+      //Test getting token
+      var user = {
+        username: 'steinar',
+        password: 'steinar'
+      }
+
+      OAuth.getAccessToken(user).then(function(response) {
+        //token = response.data
+        applyRemoteData(response.data);
+      });
     };
 
-    function applyRemoteData( userData ) {
+    function applyRemoteData(userData) {
       $scope.userData = userData;
     }
   });
--- src/app/index.js
+++ src/app/index.js
@@ -1,37 +1,44 @@
 'use strict';
 
 angular.
-  module('gainmaster', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ui.router', 'mm.foundation'])
-    .config(function ($stateProvider, $urlRouterProvider, $httpProvider) {
-      $stateProvider
-        .state('home', {
-          url: '/',
-          templateUrl: 'app/main/main.html',
-          controller: 'MainController'
-        })
-        .state('userlogin', {
-          url: '/login',
-          templateUrl: 'app/components/userlogin/userlogin.html',
-          controller: 'UserLoginController'
-        })
-        .state('measurementregister', {
-          url: '/measurementregister',
-          templateUrl: 'app/components/measurementregister/measurementregister.html',
-          controller: 'MeasurementRegisterController'
-        })
-        .state('measurementhistory', {
-          url: '/measurementhistory',
-          templateUrl: 'app/components/measurementhistory/measurementhistory.html',
-          controller: 'MeasurementHistoryController'
-        })
-        .state('userregister', {
-          url: '/register',
-          templateUrl: 'app/components/userregister/userregister.html',
-          controller: 'UserRegisterController'
-        })
-        ;
+module('gainmaster', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ui.router', 'mm.foundation', 'angular-oauth2'])
+  .config(function($stateProvider, $urlRouterProvider, $httpProvider) {
+    $stateProvider
+      .state('home', {
+        url: '/',
+        templateUrl: 'app/main/main.html',
+        controller: 'MainController'
+      })
+      .state('userlogin', {
+        url: '/login',
+        templateUrl: 'app/components/userlogin/userlogin.html',
+        controller: 'UserLoginController'
+      })
+      .state('measurementregister', {
+        url: '/measurementregister',
+        templateUrl: 'app/components/measurementregister/measurementregister.html',
+        controller: 'MeasurementRegisterController'
+      })
+      .state('measurementhistory', {
+        url: '/measurementhistory',
+        templateUrl: 'app/components/measurementhistory/measurementhistory.html',
+        controller: 'MeasurementHistoryController'
+      })
+      .state('userregister', {
+        url: '/register',
+        templateUrl: 'app/components/userregister/userregister.html',
+        controller: 'UserRegisterController'
+      });
 
-      $urlRouterProvider.otherwise('/');
-      $httpProvider.defaults.useXDomain = true;
-    })
-;
+    $urlRouterProvider.otherwise('/');
+    $httpProvider.defaults.useXDomain = true;
+  })
+  .config(['OAuthProvider', function(OAuthProvider) {
+    OAuthProvider.configure({
+      baseUrl: 'http://api.hesjevik.im',
+      clientId: 'client',
+      clientSecret: 'secret',
+      grantPath: '/oauth/token',
+      revokePath: '/oauth/revoke'
+    });
+  }]);
