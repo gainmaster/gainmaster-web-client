34e382281754e32120724a81bfcf8779284b0646
Knut Lorvik <lorviken@gmail.com>
2015-05-15 13:29:40 +0200

    Add basecontroller

--- src/app/components/mypage/mypage.controller.js
+++ src/app/components/mypage/mypage.controller.js
@@ -1,7 +1,9 @@
 'use strict';
 
 angular.module('gainmaster')
-  .controller('MypageController', function($scope, accountFactory) {
+  .controller('MypageController', function($scope, $controller, accountFactory) {
+
+    angular.extend(this, $controller('BaseController', {$scope: $scope}));
 
     accountFactory.getUserInfo('steinar').then(function(response) {
       $scope.user = response;
